2c650d9833012f5b3fa42aca570d56ed0c3021f2
Liuqing650 <1006822621@qq.com>
2018-10-31 17:20:27 +0800

    获取数组长度

--- code/index.lua
+++ code/index.lua
@@ -1,24 +1,6 @@
 -- 函数
--- 排序函数
 
--- 打印日志
-function logger(arr)
-  for k,v in pairs(arr) do
-    print(k,v)
-  end
-end
-
-names = {"Peter", "Paul", "Mary"}
-grades = {Mary = 10, Paul = 7, Peter = 8}
-table.sort(names, function (n1, n2)
-  return grades[n1] > grades[n2]
-end)
-
+-- 获取数组长度
 num = {12, 34, 5, 6, 23, 2}
-table.sort(num, function ( a, b )
-  return (a < b)
-end )
-
-logger(names);
-print('-----------------------------------------')
-logger(num);
+local n = #num -- 旧版本： table.getn(num)
+print(n)
