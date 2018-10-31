69391ac441cd3977f0e9d6401dcdee2c14c83455
Liuqing650 <1006822621@qq.com>
2018-10-31 16:50:05 +0800

    房间小游戏-函数尾调用

--- code/index.lua
+++ code/index.lua
@@ -1,17 +1,46 @@
 -- 函数
-
 -- 记忆函数
-function newCounter()
-  local i = 0;
-  return function ()
-    i = i + 1;
-    return i;
+function room1()
+  local move = io.read();
+  print("[msg]:进入 1 号房间")
+  if move == "s" then
+    return room3();
+  elseif move == "e" then
+    return room2();
+  else
+    print("[no.1]:无效的移动")
+    return room1()
   end
 end
 
-c1 = newCounter();
+function room2()
+  local move = io.read();
+  print("[msg]:进入 2 号房间")
+  if move == "s" then
+    return room4();
+  elseif move == "w" then
+    return room1();
+  else
+    print("[no.2]:无效的移动")
+    return room2()
+  end
+end
 
-print(c1())
-print(c1())
-print(c1())
-print(c1())
+function room3()
+  local move = io.read();
+  print("[msg]:进入 3 号房间")
+  if move == "n" then
+    return room1();
+  elseif move == "e" then
+    return room4();
+  else
+    print("[no.3]:无效的移动")
+    return room3()
+  end
+end
+
+function room4()
+  print("[no.4]:到达终点")
+end
+
+room1()
