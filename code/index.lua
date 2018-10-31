34967aca110853afc3b7ce2e95877a637f144df0
Liuqing650 <1006822621@qq.com>
2018-10-31 15:04:54 +0800

    for 循环

--- code/index.lua
+++ code/index.lua
@@ -1,22 +1,30 @@
--- 表的的构造
+-- for 循环
 
-days = {"Sunday", "Monday", "Tuesday", "Wednesday",     "Thursday", "Friday", "Saturday"}
-print(days[4])
+-- 数值for循环
+for i=1,10 do
+  print(i)
+end
 
-polyline = {
-  color = "blue",
-  thickness = 2,
-  npoints = 4,
-  {
-    x = 0, y = 0,
-  },
-  {
-    x = 10, y = 0,
-  },
-  {
-    x = -10, y = 10,
-  },
-};
+-- 范型for循环
+days = {
+  "Sunday", "Monday", "Tuesday", 
+  "Wednesday",     "Thursday", "Friday",
+  "Saturday"
+}
 
-print(polyline.color)
-print(polyline[2].x)
+-- revDays = {
+--   ["Sunday"] = 1,
+--   ["Monday"] = 2,
+--   ["Tuesday"] = 3,
+--   ["Wednesday"] = 4,
+--   ["Thursday"] = 5,
+--   ["Friday"] = 6,
+--   ["Saturday"] = 7
+-- }
+
+revDays = {}
+for k,v in pairs(days) do
+  revDays[v] = k
+end
+x = "Tuesday"
+print(revDays[x])
