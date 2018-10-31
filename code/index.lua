f9c6a86f19163e2e672d8c3d1ecf6a2be92555cb
Liuqing650 <1006822621@qq.com>
2018-10-31 14:11:07 +0800

    and or

--- code/index.lua
+++ code/index.lua
@@ -1,11 +1,9 @@
-function fact( n )
-  -- body
-  if n == 0 then
-    return 1
-  else
-    return n * fact(n - 1)
-  end
-end
-print("enter a number:")
-a = io.read("*number")
-print(fact(a))
+print("2" > "15")
+print(4 and 5)
+
+-- a and b   -- 如果a为false，则返回a，否则返回b
+-- a or  b   -- 如果a为true，则返回a，否则返回b 
+-- c语言： a ? b : c
+-- lua: (a and b) or c
+-- a=true -> b -> b or c
+-- a=false -> a -> a or c
