723133c0d4ab4339b78f9c5830c7a512a8dd3fd1
Liuqing650 <1006822621@qq.com>
2018-10-31 17:11:41 +0800

    函数排序:table.sort

--- code/index.lua
+++ code/index.lua
@@ -1,46 +1,24 @@
 -- 函数
--- 记忆函数
-function room1()
-  local move = io.read();
-  print("[msg]:进入 1 号房间")
-  if move == "s" then
-    return room3();
-  elseif move == "e" then
-    return room2();
-  else
-    print("[no.1]:无效的移动")
-    return room1()
+-- 排序函数
+
+-- 打印日志
+function logger(arr)
+  for k,v in pairs(arr) do
+    print(k,v)
   end
 end
 
-function room2()
-  local move = io.read();
-  print("[msg]:进入 2 号房间")
-  if move == "s" then
-    return room4();
-  elseif move == "w" then
-    return room1();
-  else
-    print("[no.2]:无效的移动")
-    return room2()
-  end
-end
+names = {"Peter", "Paul", "Mary"}
+grades = {Mary = 10, Paul = 7, Peter = 8}
+table.sort(names, function (n1, n2)
+  return grades[n1] > grades[n2]
+end)
 
-function room3()
-  local move = io.read();
-  print("[msg]:进入 3 号房间")
-  if move == "n" then
-    return room1();
-  elseif move == "e" then
-    return room4();
-  else
-    print("[no.3]:无效的移动")
-    return room3()
-  end
-end
+num = {12, 34, 5, 6, 23, 2}
+table.sort(num, function ( a, b )
+  return (a < b)
+end )
 
-function room4()
-  print("[no.4]:到达终点")
-end
-
-room1()
+logger(names);
+print('-----------------------------------------')
+logger(num);
